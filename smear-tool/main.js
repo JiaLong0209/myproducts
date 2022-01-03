0d49d74488469003e53fd64c97715a220149cbb3
JiaLong0209 <smt02091395@gmail.com>
2022-01-03 22:39:25 +0800

    add my all files

---
+++ smear-tool/main.js
@@ -0,0 +1,9 @@
+
+// var header = document.getElementByClassName('header');
+// window.addEventListener('scroll',function(){
+//     let value = window.scrollY;
+//     console.log(value);
+//     header.style.left = value *0.5 + 'px';
+
+
+// })
