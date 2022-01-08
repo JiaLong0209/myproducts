dbfcd6e0507704969cf166ab097a5adfd600dfcc
JiaLong0209 <smt02091395@gmail.com>
2022-01-08 14:41:19 +0800

    add my all files

--- smear-tool/main.js
+++ smear-tool/main.js
@@ -1,9 +1,22 @@
 
-// var img = document.getElementsByClassName("img");
-// window.addEventListener('scroll',function(){
-//     let value = window.scrollY;
-//     console.log(value);
-//     img.style.top = value *0.5 + 'px';
+
+window.onload = init();
 
 
-// })
+function init(){
+    var img = document.querySelector(".img");
+    window.addEventListener('scroll',function(){
+        let value = window.scrollY;
+        console.log(value);
+        img.style.top = value *0.3 + 'px';
+        
+        
+    });
+    console.log("start");
+    var button = document.querySelector('.button');
+    button.addEventListener('click', function(){
+        console.log("123");
+        
+    });
+    
+}
