9481e09376ab84b9124f5481b08ed17005056e71
JiaLong0209 <smt02091395@gmail.com>
2022-01-09 23:33:10 +0800

    add my all files

--- smear-tool/main.js
+++ smear-tool/main.js
@@ -4,18 +4,46 @@ window.onload = init();
 
 
 function init(){
+    // var one = document.querySelector('#one');
+    // one.addEventListener("click",()=>{
+    //     console.log("df");
+    //     document.querySelector('#photo').scrollIntoView({behavior: "auto"});
+    // })
+
     var img = document.querySelector(".img");
     window.addEventListener('scroll',function(){
         let value = window.scrollY;
-        // console.log(value);
-        img.style.top = value * 0.3 + 'px';
-        
-        
+        img.style.top = value * 0.3 + 'px'; 
     });
-    console.log("start");
-    var button = document.querySelector('.button');
-    // button.addEventListener('click', function(){
-        // console.log("123");
+    $('#one').click(()=>{
+        $('html,body').animate({
+            scrollTop:
+            $('#photo').offset().top-60
+        },500);
+        return false;
+    });
+    $('#two').click(()=>{
+        $('html,body').animate({
+            scrollTop:
+            $('#feature').offset().top-60
+        },500);
+        return false;
+    });
+    $('#three').click(()=>{
+        $('html,body').animate({
+            scrollTop:
+            $('#concept').offset().top-60
+        },500);
+        return false;
+    });
+    $('#four').click(()=>{
+        $('html,body').animate({
+            scrollTop:
+            $('#contact').offset().top-60
+        },500);
+        return false;
+    });
+    
 
-    // });
+
 }
