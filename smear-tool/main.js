10f97d361cbe243949fc44f5827fb25523357a2e
JiaLong0209 <smt02091395@gmail.com>
2022-01-09 17:51:23 +0800

    add my all files

--- smear-tool/main.js
+++ smear-tool/main.js
@@ -7,16 +7,16 @@ function init(){
     var img = document.querySelector(".img");
     window.addEventListener('scroll',function(){
         let value = window.scrollY;
-        console.log(value);
+        // console.log(value);
         img.style.top = value * 0.3 + 'px';
         
         
     });
     console.log("start");
     var button = document.querySelector('.button');
-    button.addEventListener('click', function(){
-        console.log("123");
-            
+    // button.addEventListener('click', function(){
+        // console.log("123");
+
     });
     
 }
